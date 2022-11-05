453b7d7edde5c2b21442d14756f15a0fea993099
CezzMartins <cezzmartins@gmail.com>
2022-11-05 00:36:32 -0300

    feat: create default routes and httpResponse Object

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import ip from "ip";
+import { HttpResponse } from "./domain/HttpResponse";
+import { Code } from "./enum/code.enum";
+import { Status } from "./enum/status.enum";
+
+export class App {
+    private readonly app: Application;
+    private readonly APPLICATION_RUNNING = "Application is running on: ";
+    private readonly ROUTE_NOT_FOUN = "SERVICE IS NOT FOUND!";
+
+    constructor(private readonly port: (string | number) = process.env.SERVER_PORT || 3000){
+        this.app = express(); //create server
+        this.middleWare();
+        this.routes();
+    }
+
+    listen(): void {
+        this.app.listen(this.port, () => console.log(`Server on port: ${this.port}`));
+        console.log(`${this.APPLICATION_RUNNING}, ${ip.address()}:${this.port}`);
+    }
+
+    private routes(): void {
+        this.app.use("/patients", (request: Request, response: Response) => {});
+        this.app.get("/", (_: Request, response: Response) => response.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to * PATIENTS API * version 1.0.0')));
+        this.app.all("*", (_: Request, response: Response) => response.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUN)));
+    }
+
+    private middleWare(): void {
+        this.app.use(cors({ origin: '*' }));
+        this.app.use(express.json());
+    }
+}
