e732b4245aa6242f11534b026c1dac3fb1d3f3b9
CezzMartins <cezzmartins@gmail.com>
2022-11-05 03:09:45 -0300

    feat: create routes for patients

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { createPatient, deletePatient, getPatient, getPatients, updatePatient } from "../controller/patients.controller";
 
 const patientsRoutes = Router();
+
+patientsRoutes.route('/').get(getPatients).post(createPatient);
+patientsRoutes.route('/:patientId').get(getPatient).put(updatePatient).delete(deletePatient);
 
 export default patientsRoutes;
